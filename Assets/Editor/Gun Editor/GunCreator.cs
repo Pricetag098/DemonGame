a6b8abed7d569ab3601c5d00057399618e785a3b
JacksonH <jacksonhole24@gmail.com>
2023-07-21 14:20:43 +0930

    Gun Editor and Player Pre fab updates

--- Assets/Editor/Gun Editor/GunCreator.cs
+++ Assets/Editor/Gun Editor/GunCreator.cs
@@ -33,11 +33,31 @@ public class GunCreator : EditorWindow
     {
         scrollPos = EditorGUILayout.BeginScrollView(scrollPos, GUILayout.Height(position.height));
 
-        EditorGUILayout.LabelField("Gun Information", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField("Gun Settings", EditorStyles.boldLabel);
         GUILayout.Space(10);
 
         gun.fireSelect = (Gun.FireTypes)EditorGUILayout.EnumPopup("Fire Type", gun.fireSelect, GUILayout.Width(455));
         EditorGUILayout.LabelField("The Fire Type e.g. Full Auto, Burst.", EditorStyles.miniLabel);
+
+        GUILayout.Space(5);
+
+        gun.damage = EditorGUILayout.FloatField("Damage", gun.damage, GUILayout.Width(455));
+        EditorGUILayout.LabelField("The base a damage a shot deals.", EditorStyles.miniLabel);
+
+        GUILayout.Space(5);
+
+        gun.bulletRange = EditorGUILayout.FloatField("Bullet Range", gun.bulletRange, GUILayout.Width(455));
+        EditorGUILayout.LabelField("Max range the bullet can register a hit.", EditorStyles.miniLabel);
+
+        GUILayout.Space(5);
+
+        gun.bulletSpreadDegrees = EditorGUILayout.FloatField("Bullet Spread", gun.bulletSpreadDegrees, GUILayout.Width(455));
+        EditorGUILayout.LabelField("The max bullet spread in degrees.", EditorStyles.miniLabel);
+
+        GUILayout.Space(5);
+
+        gun.bulletSpreadDegrees = EditorGUILayout.FloatField("Bullet Spread", gun.bulletSpreadDegrees, GUILayout.Width(455));
+        EditorGUILayout.LabelField("The max bullet spread in degrees.", EditorStyles.miniLabel);
 
 
 

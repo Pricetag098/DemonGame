9c0da7fd277b0a7440528734dd0f797aa0e14391
Jake Price <37175719+Pricetag098@users.noreply.github.com>
2023-07-28 00:01:37 +0930

    Ability system rework broken

---
+++ Assets/Scripts/CastingSystem/PlayerAbilityCaster.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerAbilityCaster : MonoBehaviour
+{
+    AbilityCaster caster;
+    public int activeIndex;
+    public InputActionProperty useAction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        caster = GetComponent<AbilityCaster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((useAction.action.IsPressed() && caster.abilities[activeIndex].castMode == Ability.CastModes.hold) ||
+            (useAction.action.WasPerformedThisFrame() && caster.abilities[activeIndex].castMode == Ability.CastModes.press) ||
+             caster.abilities[activeIndex].castMode == Ability.CastModes.passive)
+        {
+            caster.Cast(activeIndex, Camera.main.transform.position, Camera.main.transform.forward);
+        }
+    }
+}

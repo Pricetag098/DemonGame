590774fca42b0fb5a51fb867b60d8a3f8656fe0f
Jake Price <37175719+Pricetag098@users.noreply.github.com>
2023-09-03 13:10:32 +0930

    fixed a null reference

---
+++ Assets/Scripts/VfxTargets.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VfxTargets : MonoBehaviour
+{
+    public Optional<Transform> head;
+    public Optional<Transform> core;
+    public Optional<Transform> origin;
+}

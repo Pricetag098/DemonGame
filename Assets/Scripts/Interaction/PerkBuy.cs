61bb650ba1e03b190ffe867501d38e5ebbbe466b
Jake Price <37175719+Pricetag098@users.noreply.github.com>
2023-09-20 11:44:51 +0930

    Interavtable change

--- Assets/Scripts/Interaction/PerkBuy.cs
+++ Assets/Scripts/Interaction/PerkBuy.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 public class PerkBuy : ShopInteractable
 {
-
+	public bool upgraded;
 	[SerializeField] public Perk perk;
 	
 	protected override void DoBuy(Interactor interactor)
 	{
 		interactor.perkManager.AddPerk(Instantiate(perk));
+		if(upgraded)
+		{
+			Upgrade(interactor.perkManager);
+		}
 	}
 	
 	protected override bool CanBuy(Interactor interactor)
 	{
 		return !interactor.perkManager.HasPerk(perk);
 	}
+	
+	public void Upgrade(PerkManager perkManager)
+	{
+		if(perkManager.HasPerk(perk))
+		{
+			perkManager.GetPerk(perk).Upgrade();
+			upgraded = true;
+		}
+	}
+	[ContextMenu("Upgrade")]
+	void TestUpgrade()
+	{
+		Upgrade(FindObjectOfType<PerkManager>());
+	}
 
 	
+
 }

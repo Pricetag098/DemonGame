4870373650ab1319a0cc2f33ddf3ddee3a7675ce
Jake Price <37175719+Pricetag098@users.noreply.github.com>
2023-09-27 16:09:02 +0930

    Melee

---
+++ Assets/Scripts/CastingSystem/Abilitys/HammerAbility.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerAbility : Ability
+{
+	public override void Cast(Vector3 origin, Vector3 direction)
+	{
+		
+	}
+}

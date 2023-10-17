0b9bb42c75e946d9b762141711784d59447d3916
BlakesPage <71866323+BlakesPage@users.noreply.github.com>
2023-10-17 15:38:04 +1030

    cleaned up script

--- Assets/Scripts/Ai/Spawning/DemonAnimationOverrides.cs
+++ Assets/Scripts/Ai/Spawning/DemonAnimationOverrides.cs
@@ -9,34 +9,6 @@ public class DemonAnimationOverrides : MonoBehaviour
     private AnimatorControllerOverrides currentControllerOverride;
     private int currentOverrideIndex = 0;
 
-    private AnimatorOverrideController RandomController(Animator animator)
-    {
-        int num = Random.Range(0, animatorControllers.Count);
-
-        AnimatorControllerOverrides contoller = animatorControllers[num];
-
-        while(contoller == currentControllerOverride)
-        {
-            num = Random.Range(0, animatorControllers.Count);
-            contoller = animatorControllers[num];
-        }
-
-        num = Random.Range(0, contoller.Overrides.Count);
-
-        RuntimeAnimatorController temp = animator.runtimeAnimatorController;
-        RuntimeAnimatorController temp1 = contoller.Overrides[num];
-
-        while (temp == temp1)
-        {
-            num = Random.Range(0, animatorControllers.Count);
-            temp1 = contoller.Overrides[num];
-        }
-
-        currentControllerOverride = contoller;
-
-        return contoller.Overrides[num];
-    }
-
     public void SelectController(Animator animator)
     {
         int num = Random.Range(0, animatorControllers.Count);
@@ -67,16 +39,6 @@ public class DemonAnimationOverrides : MonoBehaviour
     {
         return RandomOverride();
     }
-
-    //public AnimatorOverrideController SetOverrideController(Animator animator)
-    //{
-    //    return RandomController(animator);
-    //}
-
-
-    // apply controller on spawn 
-    // hold reference to that controller and set override from that controller
-
 }
 
 [System.Serializable]

befe6ae3370ca95b14af21fca2f93ac5f18aad5c
Jake Price <37175719+Pricetag098@users.noreply.github.com>
2023-10-26 23:05:23 +1030

    Fix

--- Assets/PortalInteraction.cs
+++ Assets/PortalInteraction.cs
@@ -4,39 +4,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class PortalInteraction : MonoBehaviour
+public class PortalInteraction : ShopInteractable
 {
-    [SerializeField] GameObject portal;
+    [SerializeField] Transform body;
 
     [SerializeField] Vector3 minPortalSize;
 
     [SerializeField] Vector3 maxPortalSize;
 
     [SerializeField] Animator armAnimator;
+    [SerializeField] SoundPlayer openSound,idleSound,closeSound;
 
-    [SerializeField] float minEmission;
+    [SerializeField] float openTime;
 
-    [SerializeField] float maxEmission;
+    [SerializeField] Ability ability;
+    
 
-    [SerializeField] float time;
 
-    void Start()
+    public void Open()
     {
+        Sequence open = DOTween.Sequence();
+        open.Append(body.DOScale(Vector3.one, openTime)).SetEase(Ease.InSine);
+        open.AppendCallback(() => armAnimator.SetTrigger("In"));
+        open.AppendCallback(() => openSound.Play());
+		open.AppendCallback(() => idleSound.Play());
+	}
+
+   public void Close()
+    {
+		Sequence close = DOTween.Sequence();
+		close.AppendCallback(() => idleSound.Stop());
+		close.AppendCallback(() => closeSound.Play());
+		close.AppendCallback(() => armAnimator.SetTrigger("Out"));
+		close.Append(body.DOScale(Vector3.one, openTime)).SetEase(Ease.InSine);
+		
         
-    }
-
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        portal.transform.DOScale(maxPortalSize, time);
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-
-    }
+	}
 }

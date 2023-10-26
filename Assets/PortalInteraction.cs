37480a2407c0e63798ee1ab918d0e4e9e5b11302
JacksonH <jacksonhole24@gmail.com>
2023-10-26 22:20:08 +1030

    Portal Setup

---
+++ Assets/PortalInteraction.cs
@@ -0,0 +1,38 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PortalInteraction : MonoBehaviour
+{
+    [SerializeField] GameObject portal;
+
+    [SerializeField] Vector3 minPortalSize;
+
+    [SerializeField] Vector3 maxPortalSize;
+
+    [SerializeField] Animator armAnimator;
+
+    [SerializeField] float time;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        portal.transform.DOScale(maxPortalSize, time);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+    }
+}

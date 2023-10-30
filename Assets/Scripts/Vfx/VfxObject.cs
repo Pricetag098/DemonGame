954af298d79fb1cbdde52f285252065b5a3b5458
Jake Price <37175719+Pricetag098@users.noreply.github.com>
2023-10-31 00:26:46 +1030

    Mike oxmall

--- Assets/Scripts/Vfx/VfxObject.cs
+++ Assets/Scripts/Vfx/VfxObject.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class VfxObject : MonoBehaviour
 {
 	ParticleSystem[] particles;
+	VisualEffect[] effects;
 	SoundPlayer soundPlayer;
 	PooledObject pooledObject;
 	private void Awake()
 	{
 		List<ParticleSystem> list = new List<ParticleSystem>();
-		for(int i = 0; i < transform.childCount; i++)
+		List<VisualEffect> list2 = new List<VisualEffect>();
+		for (int i = 0; i < transform.childCount; i++)
 		{
 			ParticleSystem ps;
 			if(transform.GetChild(i).TryGetComponent(out ps))
 				list.Add(ps);
+			if(transform.GetChild(i).TryGetComponent(out VisualEffect vs))
+				list2.Add(vs);
+
 		}
 		particles = list.ToArray();
+		effects = list2.ToArray();
 		soundPlayer = GetComponentInChildren<SoundPlayer>();
 		pooledObject = GetComponent<PooledObject>();
 		pooledObject.OnDespawn += OnDespawn;
@@ -26,6 +33,9 @@ public class VfxObject : MonoBehaviour
 	{
 		if(particles.Length > 0)
 			particles[Random.Range(0, particles.Length)].Play();
+
+		if(effects.Length > 0)
+			effects[Random.Range(0, effects.Length)].Play();
 		soundPlayer.Play();
 	}
 
@@ -35,6 +45,9 @@ public class VfxObject : MonoBehaviour
 		{
 			p.Stop();
 		}
+		foreach(VisualEffect v in effects)
+		{ v.Stop(); }
+
 		soundPlayer.Stop();
 	}
 }

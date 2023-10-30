9f2bcfaf201eaf82ebb375e6deb1305b220511f2
JacksonH <jacksonhole24@gmail.com>
2023-10-30 23:25:13 +1030

    OMGGGGG

--- Assets/GlyphSpawning.cs
+++ Assets/GlyphSpawning.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,23 @@ public class GlyphSpawning : MonoBehaviour
 
     public float dissolveTime;
 
+    private Material dissolveMat;
+
+    private void Awake()
+    {
+        dissolveMat = abilityGlyph.GetComponentInChildren<Renderer>().sharedMaterial;
+        Debug.Log(dissolveMat.name);
+    }
+
     public void SpawnAbility()
     {
-
+        DOTween.To(() => dissolveMat.GetFloat("_Alpha_Clip"), x => dissolveMat.SetFloat("_Alpha_Clip", x), 0, dissolveTime);
+        Debug.Log("out");
     }
 
     public void DespawnAbility()
     {
-
+        DOTween.To(() => dissolveMat.GetFloat("_Alpha_Clip"), x => dissolveMat.SetFloat("_Alpha_Clip", x), 1, dissolveTime);
+        Debug.Log("in");
     }
 }

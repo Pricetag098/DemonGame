812c1eca478ba1b273405289bab10571c63bc702
BlakesPage <71866323+BlakesPage@users.noreply.github.com>
2023-11-01 14:21:52 +1030

    UPDATE
    entities now render

--- Assets/Scripts/SubSceneReferences.cs
+++ Assets/Scripts/SubSceneReferences.cs
@@ -1,20 +1,20 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using Unity.Scenes;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Scenes;
+using UnityEngine;
 
-//public class SubSceneReferences : MonoBehaviour
-//{
-//    public static SubSceneReferences Instance { get; private set; }
+public class SubSceneReferences : MonoBehaviour
+{
+    public static SubSceneReferences Instance { get; private set; }
 
-//    // ONE OF THESE FOR EACH AREA OF THE MAP
-//    public SubScene MainGate;
+    // ONE OF THESE FOR EACH AREA OF THE MAP
+    public SubScene MainGate;
 
-//    private void Awake()
-//    {
-//        if(Instance == null)
-//        {
-//            Instance = this;
-//        }
-//    }
-//}
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+}

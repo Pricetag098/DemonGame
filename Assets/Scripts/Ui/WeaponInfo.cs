2bded02fe2cffc785692fe800be08d06d23cea3e
DESKTOP-PFD60SI\mattw <s200552@students.aie.edu.au>
2023-11-03 15:32:45 +1030

    added in weapon switch tween, function is in WeaponInfo WeaponSwapTween

--- Assets/Scripts/Ui/WeaponInfo.cs
+++ Assets/Scripts/Ui/WeaponInfo.cs
@@ -2,43 +2,96 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using DG.Tweening;
+
 public class WeaponInfo : MonoBehaviour
 {
-    [SerializeField]TextMeshProUGUI clipAmountText;
-	[SerializeField] TextMeshProUGUI stashAmountText;
-	[SerializeField] TextMeshProUGUI dividerText;
-	[SerializeField] TextMeshProUGUI nameText;
-	[SerializeField] Holster holster;
+    [SerializeField] RectTransform nameParent;
+    [SerializeField] RectTransform reserveNameParent;
+    [SerializeField] TextMeshProUGUI reserveNameText;
+    bool switchtext = false;
+    public AnimationCurve easeCurve;
 
-	[SerializeField] Color defaultColour;
-	[SerializeField] Color noAmmoColour;
+    [SerializeField] TextMeshProUGUI clipAmountText;
+    [SerializeField] TextMeshProUGUI stashAmountText;
+    [SerializeField] TextMeshProUGUI dividerText;
+    [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] Holster holster;
 
-	private void Awake()
-	{
-		holster = FindObjectOfType<Holster>();
-	}
-	// Update is called once per frame
-	void Update()
+    [SerializeField] Color defaultColour;
+    [SerializeField] Color noAmmoColour;
+
+    private void Awake()
     {
+        holster = FindObjectOfType<Holster>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        nameText.text = holster.HeldGun.gunName;
+        clipAmountText.text = holster.HeldGun.ammoLeft.ToString();
+        stashAmountText.text = holster.HeldGun.stash.ToString();
+        clipAmountText.color = defaultColour;
+        stashAmountText.color = defaultColour;
+        dividerText.color = defaultColour;
 
-		nameText.text = holster.HeldGun.gunName;
-		clipAmountText.text = holster.HeldGun.ammoLeft.ToString();
-		stashAmountText.text = holster.HeldGun.stash.ToString();
-		clipAmountText.color = defaultColour;
-		stashAmountText.color = defaultColour;
-		dividerText.color = defaultColour;
-		if(holster.HeldGun.ammoLeft == 0)
+        if (holster.HeldGun.ammoLeft == 0)
         {
-			clipAmountText.color = noAmmoColour;
-		}
-		if(holster.HeldGun.stash == 0)
+            clipAmountText.color = noAmmoColour;
+        }
+        
+        if (holster.HeldGun.stash == 0)
         {
-			stashAmountText.color = noAmmoColour;
-		}
-		if(holster.HeldGun.ammoLeft == 0 && holster.HeldGun.stash == 0)
+            stashAmountText.color = noAmmoColour;
+        }
+        
+        if (holster.HeldGun.ammoLeft == 0 && holster.HeldGun.stash == 0)
         {
-			dividerText.color = noAmmoColour;
-		}
-		
+            dividerText.color = noAmmoColour;
+        }
+    }
+
+    public void WeaponSwapTween()
+    {
+        if (!switchtext)
+        {
+            WeaponSwitchTween(reserveNameParent, nameParent);
+            switchtext = true;
+        }
+        else
+        {
+            WeaponSwitchTween(nameParent, reserveNameParent);
+            switchtext = false;
+        }
+    }
+
+    void WeaponSwitchTween(RectTransform reserveText, RectTransform activeText)
+    {
+        if (!switchtext)
+        {
+            ActiveText(reserveNameText);
+            ReserveText(nameText);
+        }
+        else
+        {
+            ActiveText(nameText);
+            ReserveText(reserveNameText);
+        }
+
+        reserveText.DOAnchorPos(new Vector2(-30, 40), 0.5f).SetEase(easeCurve);
+
+        activeText.DOAnchorPos(new Vector2(0, 90), 0.5f).SetEase(easeCurve);
+    }
+
+    void ReserveText(TextMeshProUGUI reserveText)
+    {
+        reserveText.fontSize = 45;
+        reserveText.color = Color.grey;
+    }
+
+    void ActiveText(TextMeshProUGUI activeText)
+    {
+        activeText.fontSize = 50;
+        activeText.color = Color.white;
     }
 }

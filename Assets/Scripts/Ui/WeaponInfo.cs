32879c944fb53dd43d12ac61e7b7ddc7c1778d65
DESKTOP-PFD60SI\mattw <s200552@students.aie.edu.au>
2023-11-03 14:12:19 +1030

    Began tween work on weapon switch UI feedback

--- Assets/Scripts/Ui/WeaponInfo.cs
+++ Assets/Scripts/Ui/WeaponInfo.cs
@@ -2,21 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using DG.Tweening;
+
 public class WeaponInfo : MonoBehaviour
 {
     [SerializeField]TextMeshProUGUI ammoText;
-	[SerializeField] TextMeshProUGUI nameText;
-	[SerializeField] Holster holster;
+    [SerializeField] RectTransform nameParent;
+    [SerializeField] RectTransform reserveNameParent;
+    [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] TextMeshProUGUI reserveNameText;
+    [SerializeField] Holster holster;
+	bool reserveOn = false;
+
+    public AnimationCurve curve;
+    public float duration;
 
 	private void Awake()
 	{
 		holster = FindObjectOfType<Holster>();
+
+		WeaponSwitchTween();
 	}
 	// Update is called once per frame
 	void Update()
     {
-		
         ammoText.text = holster.HeldGun.ammoLeft.ToString() + "/" + holster.HeldGun.stash;
 		nameText.text = holster.HeldGun.gunName;
+    }
+
+	public void WeaponSwitchTween()
+	{
+		reserveNameParent.DOAnchorPos(new Vector2(-30, 40), holster.HeldGun.drawTime).SetEase(curve);
+        reserveNameText.fontSize = 50;
+        reserveNameText.color = Color.white;
+
+        nameParent.DOAnchorPos(new Vector2(0, 90), holster.HeldGun.drawTime).SetEase(curve);
+        nameText.fontSize = 45;
+        reserveNameText.color = Color.grey;
     }
 }

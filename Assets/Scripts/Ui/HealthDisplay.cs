7505edce17d540c81f8936b1e554a0350feb2096
Jake Price <37175719+Pricetag098@users.noreply.github.com>
2023-11-03 14:26:47 +1030

    a

--- Assets/Scripts/Ui/HealthDisplay.cs
+++ Assets/Scripts/Ui/HealthDisplay.cs
@@ -6,7 +6,7 @@ public class HealthDisplay : MonoBehaviour
 {
     Health health;
     CanvasGroup canvasGroup;
-    [SerializeField, Range(0, 1)] float startinghealth, endHealth,maxAlpha;
+    [SerializeField] float startinghealth, endHealth,maxAlpha;
     
     // Start is called before the first frame update
     void Start()

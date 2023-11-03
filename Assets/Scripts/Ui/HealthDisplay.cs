a259597d0902a44cc00a36c455ec3006a4cebe9e
Jake Price <37175719+Pricetag098@users.noreply.github.com>
2023-11-03 14:20:29 +1030

    thing

---
+++ Assets/Scripts/Ui/HealthDisplay.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CanvasGroup))]
+public class HealthDisplay : MonoBehaviour
+{
+    Health health;
+    CanvasGroup canvasGroup;
+    [SerializeField, Range(0, 1)] float startinghealth, endHealth,maxAlpha;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = FindObjectOfType<PlayerStats>().GetComponent<Health>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        canvasGroup.alpha = maxAlpha * ((health.health - startinghealth) / (endHealth - startinghealth));
+    }
+}

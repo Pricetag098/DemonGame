674d9301644ac40955b272e0fce9a03c94bd6319
Jake Price <37175719+Pricetag098@users.noreply.github.com>
2023-11-16 13:02:00 +1030

    sword

--- Assets/Scripts/CastingSystem/Abilitys/SwordAbility.cs
+++ Assets/Scripts/CastingSystem/Abilitys/SwordAbility.cs
@@ -25,39 +25,48 @@ public class SwordAbility : Ability
 	public override void Cast(Vector3 origin, Vector3 direction)
 	{
 		if (timer < cooldown)
-			return;
-		timer = 0;
-		List<Health> healths = new List<Health>();
-		slashVfx.Play(origin, direction);
-		caster.RemoveBlood(bloodCost);
-		RaycastHit[] hits = Physics.SphereCastAll(origin,rad, direction, range, layers);
-		foreach(RaycastHit hit in hits)
 		{
-			HitBox hb;
-			if(hit.collider.TryGetComponent(out hb))
+
+		}
+		else
+		{
+			timer = 0;
+			List<Health> healths = new List<Health>();
+			slashVfx.Play(origin, direction);
+			caster.RemoveBlood(bloodCost);
+			RaycastHit[] hits = Physics.SphereCastAll(origin, rad, direction, range, layers);
+			foreach (RaycastHit hit in hits)
 			{
-				if (healths.Contains(hb.health))
-					continue;
-				healths.Add(hb.health);
-				OnHit(hb.health);
-				hb.health.TakeDmg(damage * caster.DamageMulti, HitType.ABILITY);
-				if(hit.point == Vector3.zero)
+				HitBox hb;
+				if (hit.collider.TryGetComponent(out hb))
 				{
-					Vector3 pos = hit.collider.ClosestPoint(origin);
-					vfx.Play(pos, pos - origin);
+					if (healths.Contains(hb.health))
+						continue;
+					healths.Add(hb.health);
+					OnHit(hb.health);
+					hb.health.TakeDmg(damage * caster.DamageMulti, HitType.ABILITY);
+					if (hit.point == Vector3.zero)
+					{
+						Vector3 pos = hit.collider.ClosestPoint(origin);
+						vfx.Play(pos, pos - origin);
+					}
+					else
+					{
+						vfx.Play(hit.point, hit.normal);
+					}
+
 				}
-				else
-				{
-					vfx.Play(hit.point, hit.normal);
-				}
-				
 			}
 		}
+			
+		
 	}
 
 	public override void Tick()
 	{
 		timer += Time.deltaTime;
+
+		
 	}
 	public override void OnHit(Health health)
 	{

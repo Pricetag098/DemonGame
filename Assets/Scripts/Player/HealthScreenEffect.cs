029be891a0246f6e7dce548b1d632be9ba0ca7f7
JacksonH <jacksonhole24@gmail.com>
2023-11-16 19:51:53 +1030

    Updates to Vignette

--- Assets/Scripts/Player/HealthScreenEffect.cs
+++ Assets/Scripts/Player/HealthScreenEffect.cs
@@ -1,22 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
 public class HealthScreenEffect : MonoBehaviour
 {
-    [SerializeField] VolumeProfile profile;
+    [SerializeField] Volume profile;
     [SerializeField] float maxVinette = 1;
     [SerializeField] Health health;
 
+    Vignette vignette;
+
     // Update is called once per frame
+    private void Awake()
+    {
+        Vignette vignette1;
+        if (profile.profile.TryGet(out vignette1))
+        {
+            vignette = vignette1;
+        }
+    }
+
     void Update()
     {
-        Vignette vignette;
-        if (profile.TryGet(out vignette))
-        {
-            float vinetteVal = (1 - (health.health / health.maxHealth)) * maxVinette;
-            vignette.intensity.value = vinetteVal * maxVinette;
-        }
+        float vinetteVal = (1 - (health.health / health.maxHealth)) * maxVinette;
+        vignette.intensity.value = vinetteVal;
     }
 }

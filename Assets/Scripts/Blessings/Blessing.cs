0594159ba50526dbcb0af3ccc40d08152c448dc4
Jake Price <37175719+Pricetag098@users.noreply.github.com>
2023-11-30 16:02:06 +1030

    blessing

--- Assets/Scripts/Blessings/Blessing.cs
+++ Assets/Scripts/Blessings/Blessing.cs
@@ -55,6 +55,8 @@ public class Blessing : ScriptableObject
     {
         OnRemove();
         handler.activeBlessings.Remove(this);
+        Destroy(this);
+
     }
 
     protected virtual void OnRemove()

db1ada7c3b0b7233359b92eeaf826c91cb43ebe0
EUANS-PC\Euan Jones <s213321@students.aie.edu.au>
2023-12-02 16:18:50 +1030

    Updated ritual door

---
+++ Assets/Scripts/Interaction/DoorBuySpecial.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBuySpecial : DoorBuy
+{
+    //Needs to have a message that displays with no "Hold F to" only display "Complete all 3 rituals to unlock"
+    //Once all 3 have been completed "Hod F to Place soul tomb"
+    //Has NO COST
+}

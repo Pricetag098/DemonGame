843ab831361b6d7368cec0c0e54028d1059b1ad2
JacksonHoleAIE <s210837@students.aie.edu.au>
2023-12-05 14:38:38 +1030

    Has All Abilities Setup

---
+++ Assets/PlayerSetup.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSetup : MonoBehaviour
+{
+    PlayerStats playerStats;
+
+    private void Update()
+    {
+        playerStats = GetComponent<PlayerStats>();
+
+        if (GamePrefs.UnlockAllAbilities)
+        {
+            PortalInteraction[] abilBuys = FindObjectsOfType<PortalInteraction>();
+
+            int points = playerStats.points;
+            playerStats.points = 999999;
+
+            for (int i = 0; i < abilBuys.Length; i++)
+            {
+                abilBuys[i].Interact(GetComponent<Interactor>());
+            }
+            playerStats.points = points;
+        }
+        Destroy(this);
+    }
+}

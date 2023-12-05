ec518b0ca49e6fa64e917dc1ae50556090217156
Jake Price <37175719+Pricetag098@users.noreply.github.com>
2023-12-05 12:20:08 +1030

    End game ui

---
+++ Assets/Scripts/Ui/EndGameUi.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EndGameUi : MonoBehaviour
+{
+    
+
+    private void Awake()
+    {
+        
+
+    }
+    
+
+    public void Open()
+    {
+        
+    }
+
+    public void ReturnToMenu()
+    {
+
+    }
+}

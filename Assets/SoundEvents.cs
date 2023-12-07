2eb4b41a267fac81215418a945685df9581d0e8f
JacksonH <jacksonhole24@gmail.com>
2023-12-07 23:14:49 +1030

    Repeater Sound Updates

--- Assets/SoundEvents.cs
+++ Assets/SoundEvents.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GunSoundEvents : MonoBehaviour
+public class SoundEvents : MonoBehaviour
 {
     [System.Serializable]
     public class Event

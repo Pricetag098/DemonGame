a5f3710b3ad50fa1f4d7dc213d38220ad128880f
Jake Price <37175719+Pricetag098@users.noreply.github.com>
2023-12-08 01:47:49 +1030

    surface fix

--- Assets/Scripts/Vfx/Surface.cs
+++ Assets/Scripts/Vfx/Surface.cs
@@ -6,4 +6,12 @@ public class Surface : MonoBehaviour
 {
     public SurfaceData data;
 	public bool Penetrable = false;
+
+#if UNITY_EDITOR
+    private void Awake()
+    {
+        if (data == null)
+            Debug.LogError("Missing a surface data", this);
+    }
+#endif
 }

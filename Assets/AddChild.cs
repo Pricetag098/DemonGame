9670be4a9a587608c3d6ba26e4ffb8c59155bb3a
JacksonH <jacksonhole24@gmail.com>
2024-04-23 13:11:19 +0930

    Update

--- Assets/AddChild.cs
+++ Assets/AddChild.cs
@@ -6,17 +6,18 @@ public class AddChild : MonoBehaviour
 {
     [SerializeField] GameObject ball1;
     [SerializeField] GameObject ball2;
+    [SerializeField] GameObject fakeball1;
+    [SerializeField] GameObject fakeball2;
+
 
     public void AddChildren()
     {
-        Vector3 ball1pos = transform.position;
-        ball1.transform.parent = transform;
-        ball1.transform.position = ball1pos;
-        if(ball2 != null)
+        ball1.gameObject.SetActive(false);
+        fakeball1.gameObject.SetActive(true);
+        if (ball2 != null)
         {
-            Vector3 ball2pos = transform.position;
-            ball2.transform.parent = transform;
-            ball2.transform.position = ball2pos;
+            ball2.gameObject.SetActive(false);
+            fakeball2.gameObject.SetActive(true);
         }
     }
 }

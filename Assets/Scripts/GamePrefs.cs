f67bc4f9e847bd369a54d04b754f031a423d067a
Jackson Hole <jacksonhole2402@gmail.com>
2024-09-13 16:58:55 +0930

    Game

--- Assets/Scripts/GamePrefs.cs
+++ Assets/Scripts/GamePrefs.cs
@@ -7,11 +7,11 @@ public static class GamePrefs
         StartRound = round;
     }
 
-    public static int StartPoints = 999999; // set this back to 500
+    public static int StartPoints = 500; // set this back to 500
 
     public static void SetStartMoney(int amount)
     {
-        StartPoints = 999999;
+        StartPoints = amount;
     }
 
     public static bool RitualsComplete = false;
